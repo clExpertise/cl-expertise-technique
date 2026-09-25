5cc763650f0f1760a1606ab185ea8976a5e7aed9
clExpertise <cl.expertise.technique@gmail.com>
2026-09-25 17:47:58 +0200

    Adapte le site au modele Startup et retire le support

--- dist/config.js
+++
@@ -1,6 +0,0 @@
-window.CL_CONFIG = {
-  SITE_NAME: 'CL Expertise Technique',
-  API_URL: 'https://cl-support-api.clexpertise-support.workers.dev',
-  TURNSTILE_SITE_KEY: '',
-  SUPPORT_ENABLED: true
-};
